1d6b2f412b2f464adde1307780dabf3fd6af56fe
Andrei Siarzhantau <andrei_siarzhantau@epam.com>
2017-04-04 15:35:30 +0300

    code refactoring

--- app/src/main/java/com/siarzhantau/andrei/locations/SplashActivity.java
+++ app/src/main/java/com/siarzhantau/andrei/locations/SplashActivity.java
@@ -2,28 +2,16 @@ package com.siarzhantau.andrei.locations;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.os.Bundle;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
 
-import com.google.gson.Gson;
-import com.siarzhantau.andrei.locations.model.Location;
-import com.siarzhantau.andrei.locations.model.Locations;
-import com.siarzhantau.andrei.locations.utils.LocationsUtil;
+import com.hannesdorfmann.mosby.mvp.MvpActivity;
+import com.siarzhantau.andrei.locations.mvp.SplashPresenter;
+import com.siarzhantau.andrei.locations.mvp.SplashView;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.UUID;
-
-
-import io.realm.Realm;
-
-public class SplashActivity extends AppCompatActivity {
-
-    private SharedPreferences mPrefs;
+public class SplashActivity extends MvpActivity<SplashView, SplashPresenter>
+        implements SplashView  {
 
     @SuppressLint("ApplySharedPref")
     @Override
@@ -31,53 +19,18 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        mPrefs = getSharedPreferences("com.siarzhantau.andrei.locations", MODE_PRIVATE);
-
-        if (mPrefs.getBoolean("firstRun", true)) {
-            mPrefs.edit().putBoolean("firstRun", false).commit();
-
-            new AsyncTask<Void, Void, Void>() {
-                @Override
-                protected Void doInBackground(Void... voids) {
-                    populateRealm();
-                    return null;
-                }
-
-                @Override
-                protected void onPostExecute(Void aVoid) {
-                    startLocationsActivity();
-                    finish();
-                }
-            }.execute();
-        } else {
-            startLocationsActivity();
-            finish();
-        }
+        presenter.checkFirstRun(getApplicationContext());
     }
 
-    private void populateRealm() {
-        Locations locations = null;
-        try {
-            locations = new Gson().fromJson(new BufferedReader(
-                    new InputStreamReader(getAssets().open("locations.json"), "UTF-8")), Locations.class);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        if (locations != null) {
-            Realm realm = Realm.getDefaultInstance();
-            for (Location location : locations.locations) {
-                realm.executeTransaction(r -> {
-                    location.id = UUID.randomUUID().toString();
-                    location.distance = LocationsUtil.calculateDistance(
-                            location.name, location.lat, location.lng);
-                    r.copyToRealm(location);
-                });
-            }
-        }
+    @NonNull
+    @Override
+    public SplashPresenter createPresenter() {
+        return new SplashPresenter();
     }
 
-    private void startLocationsActivity() {
+    @Override
+    public void goToNextScreen() {
         startActivity(new Intent(this, LocationsActivity.class));
+        finish();
     }
 }
---
+++ app/src/main/java/com/siarzhantau/andrei/locations/mvp/SplashPresenter.java
@@ -0,0 +1,70 @@
+package com.siarzhantau.andrei.locations.mvp;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+
+import com.google.gson.Gson;
+import com.hannesdorfmann.mosby.mvp.MvpBasePresenter;
+import com.siarzhantau.andrei.locations.model.Location;
+import com.siarzhantau.andrei.locations.model.Locations;
+import com.siarzhantau.andrei.locations.utils.LocationsUtil;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.UUID;
+
+import io.realm.Realm;
+
+public class SplashPresenter extends MvpBasePresenter<SplashView> {
+
+    public void checkFirstRun(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("com.siarzhantau.andrei.locations", Context.MODE_PRIVATE);
+
+        if (prefs.getBoolean("firstRun", true)) {
+            prefs.edit().putBoolean("firstRun", false).commit();
+
+            new AsyncTask<Void, Void, Void>() {
+                @Override
+                protected Void doInBackground(Void... voids) {
+                    populateRealm(context);
+                    return null;
+                }
+
+                @Override
+                protected void onPostExecute(Void aVoid) {
+                    if (isViewAttached() && getView() != null) {
+                        getView().goToNextScreen();
+                    }
+                }
+            }.execute();
+        } else {
+            if (isViewAttached() && getView() != null) {
+                getView().goToNextScreen();
+            }
+        }
+    }
+
+    private void populateRealm(Context context) {
+        Locations locations = null;
+        try {
+            locations = new Gson().fromJson(new BufferedReader(
+                    new InputStreamReader(context.getAssets().open("locations.json"), "UTF-8")), Locations.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (locations != null) {
+            Realm realm = Realm.getDefaultInstance();
+            for (Location location : locations.locations) {
+                realm.executeTransaction(r -> {
+                    location.id = UUID.randomUUID().toString();
+                    location.distance = LocationsUtil.calculateDistance(
+                            location.name, location.lat, location.lng);
+                    r.copyToRealm(location);
+                });
+            }
+        }
+    }
+}
